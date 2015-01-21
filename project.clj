b556227cb0e1866351965dd9fad9b6943e806637
Tom Crayford <tcrayford@googlemail.com>
2015-01-21 14:51:15 +0000

    bump version number for release

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject yeller-timbre-appender "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject yeller-timbre-appender "0.4.1"
+  :description "A timbre appender that sends exceptions to yellerapp.com"
+  :url "https://github.com/yeller/yeller-timbre-appender"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.taoensso/timbre "3.2.1"]
-                 [yeller-clojure-client "0.2.0-SNAPSHOT"]])
+                 [yeller-clojure-client "0.4.1"]])
