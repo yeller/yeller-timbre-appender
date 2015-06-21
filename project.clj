d58d1d6149e7657afd69cb51616a24e96035f75f
Tom Crayford <tcrayford@googlemail.com>
2015-06-21 19:30:40 +0100

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yeller-timbre-appender "1.2.0"
+(defproject yeller-timbre-appender "2.0.0"
   :description "A timbre appender that sends exceptions to yellerapp.com"
   :url "https://github.com/yeller/yeller-timbre-appender"
   :license {:name "Eclipse Public License"
